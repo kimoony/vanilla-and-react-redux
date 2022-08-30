c58d07e5d25a2caecb31524978b0d4e044576928
kimoony <hoonby9011@gmail.com>
2022-08-30 16:30:41 +0900

    createActioin

--- react-redux/src/components/ToDo.js
+++ react-redux/src/components/ToDo.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { deleteToDo } from '../store';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { deleteToDo } from "../store";
 
 function ToDo({ toDo }) {
   const dispatch = useDispatch();
 
   const onDelete = () => {
-    dispatch(deleteToDo(toDo.id))
-  }
+    dispatch(deleteToDo(toDo.id));
+  };
 
   return (
-    <div style={{ display: 'flex' }}>
-      <Link to={`/${toDo.id}`}>
-        {toDo.text}<button onClick={onDelete}>삭제</button>
-      </Link>
+    <div style={{ display: "flex" }}>
+      <Link to={`/${toDo.id}`}>{toDo.text}</Link>
+      <button onClick={onDelete}>삭제</button>
     </div>
-  )
+  );
 }
 
-export default ToDo
+export default ToDo;
--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,35 +1,40 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// export const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text
+//   }
+// }
 
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text
-  }
-}
+// export const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id
+//   }
+// }
 
-export const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id
-  }
-}
+export const addToDo = createAction("ADD");
+export const deleteToDo = createAction("DELETE");
 
+console.log(addToDo(), deleteToDo());
 
 const reducer = (state = [], action) => {
-  console.log(state)
+  console.log(state);
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state]
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id)
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
-      return state
+      return state;
   }
-}
+};
+console.log(addToDo(), deleteToDo());
 
 const store = createStore(reducer);
 
